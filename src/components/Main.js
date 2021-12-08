a519e16e0a5e8f800c174df34df9d61b73125609
s2100688 <89017210+s2100688@users.noreply.github.com>
2021-12-08 10:46:08 +0200

    error fixing

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,69 +1,64 @@
 import React, { Component } from "react";
 
 class Main extends Component {
-    state = {
-        counter: 0,
-    }
+  state = {
+    counter: 0,
+  };
 
-    addFiveHandler = () => {
-        this.setState({
-            counter: this.state.counter + 5
-        })
-    }
+  addFiveHandler = () => {
+    this.setState({
+      counter: this.state.counter + 5,
+    });
+  };
 
-    addOneHandler = () => {
-        this.setState({
-            counter: this.state.counter + 1
-        })
-    }
+  addOneHandler = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  };
 
-    removeOneHandler = () => {
-        this.setState({
-            counter: this.state.counter - 1 
-        })
-    }
+  removeOneHandler = () => {
+    this.setState({
+      counter: this.state.counter - 1,
+    });
+  };
 
-    removeFiveHandler = () => {
-        this.setState({
-            counter: this.state.counter - 5
-        })
-    }
+  removeFiveHandler = () => {
+    this.setState({
+      counter: this.state.counter - 5,
+    });
+  };
 
-    resetHandler = () => {
-        this.setState({
-            counter: this.state.counter = 0
-        })
-    }
+  resetHandler = () => {
+    this.setState({
+      counter: 0,
+    });
+  };
 
-    
+  render() {
+    let circleClass = "";
+    this.state.counter === 0
+      ? (circleClass = "circle")
+      : this.state.counter % 2 === 0
+      ? (circleClass = "circle even")
+      : (circleClass = "circle odd");
 
-    render() {
-        let circleClass = "";
-        this.state.counter === 0
-            ? (circleClass = "circle")
-            : this.state.counter % 2 === 0
-            ? (circleClass = "circle even")
-            : (circleClass = "circle odd")
-
-
-        return (
-            <main>
-          <div className={circleClass}>
-              <p id="counter">{this.state.counter}</p>
-          </div>
+    return (
+      <main>
+        <div className={circleClass}>
+          <p id="counter">{this.state.counter}</p>
+        </div>
 
         <div className="buttonContainer">
-            <button onClick={this.addFiveHandler}>Add Five</button>
-            <button onClick={this.addOneHandler}>Add One</button>
-            <button onClick={this.removeOneHandler}>Remove One</button>
-            <button onClick={this.removeFiveHandler}>Remove Five</button>
-            
+          <button onClick={this.addFiveHandler}>Add Five</button>
+          <button onClick={this.addOneHandler}>Add One</button>
+          <button onClick={this.removeOneHandler}>Remove One</button>
+          <button onClick={this.removeFiveHandler}>Remove Five</button>
         </div>
         <button onClick={this.resetHandler}>Reset</button>
       </main>
-
-        )
-    }
+    );
+  }
 }
 
 export default Main;
